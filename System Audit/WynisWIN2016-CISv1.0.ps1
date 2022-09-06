d028cc55d0aba1c349435f77d5fe363e353ea8da
Zyaire <zyairelai@gmail.com>
2022-09-07 01:02:07 +0800

    Remove ASCII

--- System Audit/WynisWIN2016-CISv1.0.ps1
+++ System Audit/WynisWIN2016-CISv1.0.ps1
@@ -1,54 +1,8 @@
 #Requires -RunAsAdministrator
 #Requires -Version 4.0
-#Author:Sneakysecdoggo
-#Be awesome send me cookie
-#This script must be run with admin rights 
-#Check Windows Security Best Practice CIS 
-#https://github.com/Sneakysecdoggo/
-#https://twitter.com/SneakyWafWaf
-#Script Server Version
-#MIT License
 
-#Copyright (c) [2019] [Sneakysecdoggo]
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE
-#For running Prod , for debug comment the ligne below
 $ErrorActionPreference= 'silentlycontinue'
 ##########
-
-#ASCII ART BITCH
-
-Write-Host " __          __          _      "  -ForegroundColor Cyan 
-Write-Host " \ \        / /         (_)     "  -ForegroundColor Cyan 
-Write-Host "  \ \  /\  / /   _ _ __  _ ___  "  -ForegroundColor Cyan 
-Write-Host "   \ \/  \/ / | | | '_ \| / __| "  -ForegroundColor Cyan 
-Write-Host "    \  /\  /| |_| | | | | \__ \ "  -ForegroundColor Cyan 
-Write-Host "     \/  \/  \__, |_| |_|_|___/ "  -ForegroundColor Cyan 
-Write-Host "              __/ |             "  -ForegroundColor Cyan 
-Write-Host "             |___/              "  -ForegroundColor Cyan 
-Write-Host " __          _______ ___   ___  __   __ "  -ForegroundColor Black 
-Write-Host " \ \        / / ____|__ \ / _ \/_ | / /"  -ForegroundColor Black 
-Write-Host "  \ \  /\  / / (___    ) | | | || |/ /_"  -ForegroundColor Black  
-Write-Host "   \ \/  \/ / \___ \  / /| | | || | '_ \"  -ForegroundColor Black 
-Write-Host "    \  /\  /  ____) |/ /_| |_| || | (_) | "  -ForegroundColor Black 
-Write-Host "     \/  \/  |_____/|____|\___/ |_|\___/ "  -ForegroundColor Black 
-#FUNCTION 
 
 $reverveCommand=Get-Command | Where-Object { $_.name -match "Get-WSManInstance"}
 if($reverveCommand -ne $null){
